23c18ac8f232b1057ae39e2d19aa6e8167212e48
sutharsan-g <gsutharsan29@gmail.com>
2024-11-22 16:05:21 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR {import.meta.env.VITE_GH_TOKEN ?? "No env found"}
         </p>
       </div>
       <p className="read-the-docs">
