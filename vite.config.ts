e55f477684a8e2c92f5931bee655b5c094f2415c
sutharsan-g <gsutharsan29@gmail.com>
2024-11-22 16:45:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base:"/vite-react-ts",
-  root: './',  // Ensures Vite uses the root directory for serving
-  build: {
-    outDir: 'dist',  // Ensure this is where you want the build output
-    rollupOptions: {
-      input: 'index.html',  // Ensure it's pointing to the correct entry point
-    }
-  }
+  build: { 
+    rollupOptions: { input: 'production/index.html', }, 
+  },
 })
