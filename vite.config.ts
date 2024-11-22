c239d6a9790803f066d429ed6998345f2d1ef502
sutharsan-g <gsutharsan29@gmail.com>
2024-11-22 16:31:21 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/vite-react-ts"
+  base:"/vite-react-ts",
+   build: {
+    rollupOptions: {
+      input: 'index.html',  
+    },
+    outDir: 'dist', 
+  },
 })
